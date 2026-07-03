de714b6a1dc6dcb2a7c31833768594fa9f9c29c2
jamesbond09REAL <nonono>
2026-07-03 23:17:44 +0100

    Add project files.

---
+++ Jamiro-match/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace Jamiro_match
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.orangebackj1 = new System.Windows.Forms.Button();
+            this.orangebackj2 = new System.Windows.Forms.Button();
+            this.peakyj1 = new System.Windows.Forms.Button();
+            this.peakyj2 = new System.Windows.Forms.Button();
+            this.thegang1 = new System.Windows.Forms.Button();
+            this.thegang2 = new System.Windows.Forms.Button();
+            this.manwearing1 = new System.Windows.Forms.Button();
+            this.manwearing2 = new System.Windows.Forms.Button();
+            this.snailmail1 = new System.Windows.Forms.Button();
+            this.snailmail2 = new System.Windows.Forms.Button();
+            this.tuffaura1 = new System.Windows.Forms.Button();
+            this.tuffaura2 = new System.Windows.Forms.Button();
+            this.snailcover1 = new System.Windows.Forms.Button();
+            this.gangcover1 = new System.Windows.Forms.Button();
+            this.ocover1 = new System.Windows.Forms.Button();
+            this.gangcover2 = new System.Windows.Forms.Button();
+            this.snailcover2 = new System.Windows.Forms.Button();
+            this.ocover2 = new System.Windows.Forms.Button();
+            this.tuffcover1 = new System.Windows.Forms.Button();
+            this.peakycover1 = new System.Windows.Forms.Button();
+            this.peakycover2 = new System.Windows.Forms.Button();
+            this.tuffcover2 = new System.Windows.Forms.Button();
+            this.guycover2 = new System.Windows.Forms.Button();
+            this.guycover1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // orangebackj1
+            // 
+            this.orangebackj1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.orangebackj1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("orangebackj1.BackgroundImage")));
+            this.orangebackj1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.orangebackj1.Location = new System.Drawing.Point(346, 92);
+            this.orangebackj1.Name = "orangebackj1";
+            this.orangebackj1.Size = new System.Drawing.Size(59, 126);
+            this.orangebackj1.TabIndex = 0;
+            this.orangebackj1.UseVisualStyleBackColor = false;
+            // 
+            // orangebackj2
+            // 
+            this.orangebackj2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("orangebackj2.BackgroundImage")));
+            this.orangebackj2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.orangebackj2.Location = new System.Drawing.Point(411, 92);
+            this.orangebackj2.Name = "orangebackj2";
+            this.orangebackj2.Size = new System.Drawing.Size(59, 126);
+            this.orangebackj2.TabIndex = 1;
+            this.orangebackj2.UseVisualStyleBackColor = true;
+            // 
+            // peakyj1
+            // 
+            this.peakyj1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("peakyj1.BackgroundImage")));
+            this.peakyj1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.peakyj1.Location = new System.Drawing.Point(476, 92);
+            this.peakyj1.Name = "peakyj1";
+            this.peakyj1.Size = new System.Drawing.Size(59, 126);
+            this.peakyj1.TabIndex = 2;
+            this.peakyj1.UseVisualStyleBackColor = true;
+            // 
+            // peakyj2
+            // 
+            this.peakyj2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("peakyj2.BackgroundImage")));
+            this.peakyj2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.peakyj2.Location = new System.Drawing.Point(541, 92);
+            this.peakyj2.Name = "peakyj2";
+            this.peakyj2.Size = new System.Drawing.Size(59, 126);
+            this.peakyj2.TabIndex = 3;
+            this.peakyj2.UseVisualStyleBackColor = true;
+            // 
+            // thegang1
+            // 
+            this.thegang1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("thegang1.BackgroundImage")));
+            this.thegang1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.thegang1.Location = new System.Drawing.Point(346, 224);
+            this.thegang1.Name = "thegang1";
+            this.thegang1.Size = new System.Drawing.Size(59, 126);
+            this.thegang1.TabIndex = 4;
+            this.thegang1.UseVisualStyleBackColor = true;
+            // 
+            // thegang2
+            // 
+            this.thegang2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("thegang2.BackgroundImage")));
+            this.thegang2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.thegang2.Location = new System.Drawing.Point(411, 224);
+            this.thegang2.Name = "thegang2";
+            this.thegang2.Size = new System.Drawing.Size(59, 126);
+            this.thegang2.TabIndex = 5;
+            this.thegang2.UseVisualStyleBackColor = true;
+            // 
+            // manwearing1
+            // 
+            this.manwearing1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("manwearing1.BackgroundImage")));
+            this.manwearing1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.manwearing1.Location = new System.Drawing.Point(476, 224);
+            this.manwearing1.Name = "manwearing1";
+            this.manwearing1.Size = new System.Drawing.Size(59, 126);
+            this.manwearing1.TabIndex = 6;
+            this.manwearing1.UseVisualStyleBackColor = true;
+            // 
+            // manwearing2
+            // 
+            this.manwearing2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("manwearing2.BackgroundImage")));
+            this.manwearing2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.manwearing2.Location = new System.Drawing.Point(541, 224);
+            this.manwearing2.Name = "manwearing2";
+            this.manwearing2.Size = new System.Drawing.Size(59, 126);
+            this.manwearing2.TabIndex = 7;
+            this.manwearing2.UseVisualStyleBackColor = true;
+            // 
+            // snailmail1
+            // 
+            this.snailmail1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("snailmail1.BackgroundImage")));
+            this.snailmail1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.snailmail1.Location = new System.Drawing.Point(346, 356);
+            this.snailmail1.Name = "snailmail1";
+            this.snailmail1.Size = new System.Drawing.Size(59, 126);
+            this.snailmail1.TabIndex = 8;
+            this.snailmail1.UseVisualStyleBackColor = true;
+            // 
+            // snailmail2
+            // 
+            this.snailmail2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("snailmail2.BackgroundImage")));
+            this.snailmail2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.snailmail2.Location = new System.Drawing.Point(411, 356);
+            this.snailmail2.Name = "snailmail2";
+            this.snailmail2.Size = new System.Drawing.Size(59, 126);
+            this.snailmail2.TabIndex = 9;
+            this.snailmail2.UseVisualStyleBackColor = true;
+            // 
+            // tuffaura1
+            // 
+            this.tuffaura1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tuffaura1.BackgroundImage")));
+            this.tuffaura1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tuffaura1.Location = new System.Drawing.Point(476, 356);
+            this.tuffaura1.Name = "tuffaura1";
+            this.tuffaura1.Size = new System.Drawing.Size(59, 126);
+            this.tuffaura1.TabIndex = 10;
+            this.tuffaura1.UseVisualStyleBackColor = true;
+            // 
+            // tuffaura2
+            // 
+            this.tuffaura2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tuffaura2.BackgroundImage")));
+            this.tuffaura2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tuffaura2.Location = new System.Drawing.Point(541, 356);
+            this.tuffaura2.Name = "tuffaura2";
+            this.tuffaura2.Size = new System.Drawing.Size(59, 126);
+            this.tuffaura2.TabIndex = 11;
+            this.tuffaura2.UseVisualStyleBackColor = true;
+            // 
+            // snailcover1
+            // 
+            this.snailcover1.Location = new System.Drawing.Point(346, 356);
+            this.snailcover1.Name = "snailcover1";
+            this.snailcover1.Size = new System.Drawing.Size(59, 126);
+            this.snailcover1.TabIndex = 12;
+            this.snailcover1.UseVisualStyleBackColor = true;
+            this.snailcover1.Click += new System.EventHandler(this.snail1_Click);
+            // 
+            // gangcover1
+            // 
+            this.gangcover1.Location = new System.Drawing.Point(346, 224);
+            this.gangcover1.Name = "gangcover1";
+            this.gangcover1.Size = new System.Drawing.Size(59, 126);
+            this.gangcover1.TabIndex = 13;
+            this.gangcover1.UseVisualStyleBackColor = true;
+            this.gangcover1.Click += new System.EventHandler(this.gangcover1_Click);
+            // 
+            // ocover1
+            // 
+            this.ocover1.Location = new System.Drawing.Point(346, 92);
+            this.ocover1.Name = "ocover1";
+            this.ocover1.Size = new System.Drawing.Size(59, 126);
+            this.ocover1.TabIndex = 14;
+            this.ocover1.UseVisualStyleBackColor = true;
+            this.ocover1.Click += new System.EventHandler(this.ocover1_Click);
+            // 
+            // gangcover2
+            // 
+            this.gangcover2.Location = new System.Drawing.Point(411, 224);
+            this.gangcover2.Name = "gangcover2";
+            this.gangcover2.Size = new System.Drawing.Size(59, 126);
+            this.gangcover2.TabIndex = 15;
+            this.gangcover2.UseVisualStyleBackColor = true;
+            this.gangcover2.Click += new System.EventHandler(this.gangcover2_Click);
+            // 
+            // snailcover2
+            // 
+            this.snailcover2.Location = new System.Drawing.Point(411, 356);
+            this.snailcover2.Name = "snailcover2";
+            this.snailcover2.Size = new System.Drawing.Size(59, 126);
+            this.snailcover2.TabIndex = 16;
+            this.snailcover2.UseVisualStyleBackColor = true;
+            this.snailcover2.Click += new System.EventHandler(this.snail2_Click);
+            // 
+            // ocover2
+            // 
+            this.ocover2.Location = new System.Drawing.Point(411, 92);
+            this.ocover2.Name = "ocover2";
+            this.ocover2.Size = new System.Drawing.Size(59, 126);
+            this.ocover2.TabIndex = 17;
+            this.ocover2.UseVisualStyleBackColor = true;
+            this.ocover2.Click += new System.EventHandler(this.ocover2_Click);
+            // 
+            // tuffcover1
+            // 
+            this.tuffcover1.Location = new System.Drawing.Point(476, 356);
+            this.tuffcover1.Name = "tuffcover1";
+            this.tuffcover1.Size = new System.Drawing.Size(59, 126);
+            this.tuffcover1.TabIndex = 18;
+            this.tuffcover1.UseVisualStyleBackColor = true;
+            this.tuffcover1.Click += new System.EventHandler(this.tuff1_Click);
+            // 
+            // peakycover1
+            // 
+            this.peakycover1.Location = new System.Drawing.Point(476, 92);
+            this.peakycover1.Name = "peakycover1";
+            this.peakycover1.Size = new System.Drawing.Size(59, 126);
+            this.peakycover1.TabIndex = 19;
+            this.peakycover1.UseVisualStyleBackColor = true;
+            this.peakycover1.Click += new System.EventHandler(this.peakycover1_Click);
+            // 
+            // peakycover2
+            // 
+            this.peakycover2.Location = new System.Drawing.Point(541, 92);
+            this.peakycover2.Name = "peakycover2";
+            this.peakycover2.Size = new System.Drawing.Size(59, 126);
+            this.peakycover2.TabIndex = 20;
+            this.peakycover2.UseVisualStyleBackColor = true;
+            this.peakycover2.Click += new System.EventHandler(this.peakycover2_Click);
+            // 
+            // tuffcover2
+            // 
+            this.tuffcover2.Location = new System.Drawing.Point(541, 356);
+            this.tuffcover2.Name = "tuffcover2";
+            this.tuffcover2.Size = new System.Drawing.Size(59, 126);
+            this.tuffcover2.TabIndex = 21;
+            this.tuffcover2.UseVisualStyleBackColor = true;
+            this.tuffcover2.Click += new System.EventHandler(this.tuff2_Click);
+            // 
+            // guycover2
+            // 
+            this.guycover2.Location = new System.Drawing.Point(541, 224);
+            this.guycover2.Name = "guycover2";
+            this.guycover2.Size = new System.Drawing.Size(59, 126);
+            this.guycover2.TabIndex = 22;
+            this.guycover2.UseVisualStyleBackColor = true;
+            this.guycover2.Click += new System.EventHandler(this.guycover2_Click);
+            // 
+            // guycover1
+            // 
+            this.guycover1.Location = new System.Drawing.Point(476, 224);
+            this.guycover1.Name = "guycover1";
+            this.guycover1.Size = new System.Drawing.Size(59, 126);
+            this.guycover1.TabIndex = 23;
+            this.guycover1.UseVisualStyleBackColor = true;
+            this.guycover1.Click += new System.EventHandler(this.guycover1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 570);
+            this.Controls.Add(this.guycover1);
+            this.Controls.Add(this.guycover2);
+            this.Controls.Add(this.tuffcover2);
+            this.Controls.Add(this.peakycover2);
+            this.Controls.Add(this.peakycover1);
+            this.Controls.Add(this.tuffcover1);
+            this.Controls.Add(this.ocover2);
+            this.Controls.Add(this.snailcover2);
+            this.Controls.Add(this.gangcover2);
+            this.Controls.Add(this.ocover1);
+            this.Controls.Add(this.gangcover1);
+            this.Controls.Add(this.snailcover1);
+            this.Controls.Add(this.tuffaura2);
+            this.Controls.Add(this.tuffaura1);
+            this.Controls.Add(this.snailmail2);
+            this.Controls.Add(this.snailmail1);
+            this.Controls.Add(this.manwearing2);
+            this.Controls.Add(this.manwearing1);
+            this.Controls.Add(this.thegang2);
+            this.Controls.Add(this.thegang1);
+            this.Controls.Add(this.peakyj2);
+            this.Controls.Add(this.peakyj1);
+            this.Controls.Add(this.orangebackj2);
+            this.Controls.Add(this.orangebackj1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button orangebackj1;
+        private System.Windows.Forms.Button orangebackj2;
+        private System.Windows.Forms.Button peakyj1;
+        private System.Windows.Forms.Button peakyj2;
+        private System.Windows.Forms.Button thegang1;
+        private System.Windows.Forms.Button thegang2;
+        private System.Windows.Forms.Button manwearing1;
+        private System.Windows.Forms.Button manwearing2;
+        private System.Windows.Forms.Button snailmail1;
+        private System.Windows.Forms.Button snailmail2;
+        private System.Windows.Forms.Button tuffaura1;
+        private System.Windows.Forms.Button tuffaura2;
+        private System.Windows.Forms.Button snailcover1;
+        private System.Windows.Forms.Button gangcover1;
+        private System.Windows.Forms.Button ocover1;
+        private System.Windows.Forms.Button gangcover2;
+        private System.Windows.Forms.Button snailcover2;
+        private System.Windows.Forms.Button ocover2;
+        private System.Windows.Forms.Button tuffcover1;
+        private System.Windows.Forms.Button peakycover1;
+        private System.Windows.Forms.Button peakycover2;
+        private System.Windows.Forms.Button tuffcover2;
+        private System.Windows.Forms.Button guycover2;
+        private System.Windows.Forms.Button guycover1;
+    }
+}
+
---
+++ Jamiro-match/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jamiro_match
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int orange = 0;
+        public int peaky = 0;
+        public int gang = 0;
+        public int guy = 0;
+        public int snail = 0;
+        public int tuff = 0;
+        public bool checker(string which, int value)
+        {
+            return true;
+        }
+
+        private void ocover1_Click(object sender, EventArgs e)
+        {
+            ocover1.Visible= false;
+        }
+
+        private void ocover2_Click(object sender, EventArgs e)
+        {
+            ocover2.Visible = false;
+        }
+
+        private void peakycover1_Click(object sender, EventArgs e)
+        {
+            peakycover1.Visible = false;
+        }
+
+        private void peakycover2_Click(object sender, EventArgs e)
+        {
+            peakycover2.Visible = false;
+        }
+
+        private void gangcover1_Click(object sender, EventArgs e)
+        {
+            gangcover1.Visible = false;
+        }
+
+        private void gangcover2_Click(object sender, EventArgs e)
+        {
+            gangcover2.Visible = false;
+        }
+
+        private void guycover1_Click(object sender, EventArgs e)
+        {
+            guycover1.Visible = false;
+        }
+
+        private void guycover2_Click(object sender, EventArgs e)
+        {
+            guycover2.Visible = false;
+        }
+
+        private void snail1_Click(object sender, EventArgs e)
+        {
+            snailcover1.Visible = false;
+        }
+
+        private void snail2_Click(object sender, EventArgs e)
+        {
+            snailcover2.Visible = false;
+        }
+
+        private void tuff1_Click(object sender, EventArgs e)
+        {
+            tuffcover1.Visible = false;
+        }
+
+        private void tuff2_Click(object sender, EventArgs e)
+        {
+            tuffcover2.Visible = false;
+        }
+    }
+}
