701af199ddbf7ebd34c32d8e4ba40a9ca35897e0
jamesbond09REAL <nonono>
2026-07-04 01:16:19 +0100

    ok this time for realsies

--- Jamiro-match/Form1.Designer.cs
+++ Jamiro-match/Form1.Designer.cs
@@ -330,6 +330,7 @@
             // 
             // textBox1
             // 
+            this.textBox1.Enabled = false;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(94, 27);
             this.textBox1.Name = "textBox1";
